1e29be2317dcb8b4a56b8a26526c6d415469aed5
杉山貴哉 <s.takaya1027@gmail.com>
2019-09-03 16:22:35 +0900

    Favoriteテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_031013) do
+ActiveRecord::Schema.define(version: 2019_09_03_072136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_favorites_on_photo_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "photo"
