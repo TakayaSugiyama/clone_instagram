41881fed69ff58fecae3300e975af5f4b22bc48c
杉山貴哉 <s.takaya1027@gmail.com>
2019-09-05 16:01:12 +0900

    フォロー・フォロワー機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_072136) do
+ActiveRecord::Schema.define(version: 2019_09_05_063443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,26 @@ ActiveRecord::Schema.define(version: 2019_09_03_072136) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "relations", force: :cascade do |t|
+    t.integer "follow_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id", "followed_id"], name: "index_relations_on_follow_id_and_followed_id", unique: true
+    t.index ["follow_id"], name: "index_relations_on_follow_id"
+    t.index ["followed_id"], name: "index_relations_on_followed_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
